8c3d6c6c6c18be7b0d4f040052a591d0824246af
Justin Grevich <justin@grevi.ch>
2014-02-01 14:50:06 -0800

    basic scaffold for layout.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def search
+    redirect_to root_url, notice: "Yay"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 OzpinGen::Application.routes.draw do
   root :to => "home#index"
+  get '/search', :to =>'home#search'
 end
